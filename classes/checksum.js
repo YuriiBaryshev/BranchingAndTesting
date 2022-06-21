72f4b875a0b700b6e5bae23117dbac20a9bfac22
Scofield-04-21-076 <vitaliy.kazmirevskiy@elephantslab.com>
2022-06-21 18:04:11 +0300

    add class Checksum

---
+++ classes/checksum.js
@@ -0,0 +1,42 @@
+class Checksum {
+
+  // 4bits -> 5bits
+  static encode(dataToEncode) {
+    if(dataToEncode.length != 4) {
+      throw Error("Checksum: encode input must have exactly 4 bits");
+    }
+
+    for(let i = 0; i < 4; i++) {
+      if((dataToEncode[i] > 1)||(dataToEncode[i] == null)||(dataToEncode[i].toString() == 'undefined')) {
+        throw Error("Checksum: encode input must consist of bits");
+      }
+    }
+
+    let checksumEncoded = [1,1,1,1,1];
+
+    let parity = 0
+    for(let i = 0; i < 4; i++) {
+        if(dataToEncode[i] == 1) {
+            parity++;
+     }
+    }
+
+    if(dataToEncode.length % 2 == 0) {
+       checksumEncoded[0] = dataToEncode[0];
+       checksumEncoded[1] = dataToEncode[1];
+       checksumEncoded[2] = dataToEncode[2];
+       checksumEncoded[3] = dataToEncode[3];
+       checksumEncoded[4] = 1;
+    } else {
+         checksumEncoded[0] = dataToEncode[0];
+         checksumEncoded[1] = dataToEncode[1];
+         checksumEncoded[2] = dataToEncode[2];
+         checksumEncoded[3] = dataToEncode[3];
+         checksumEncoded[4] = 0;
+    }
+
+    return checksumEncoded;
+  }
+}
+
+module.exports = Checksum;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let hamming = require('./classes/hamming.js');
+let checksum = require('./classes/checksum.js');
 
 module.exports = hamming;
